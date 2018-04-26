6fb7ec199e0654a638ebd707488e2fce99edd82a
alemesh <alemesh@acceptic.com>
2018-04-26 19:01:04 +0300

    finsh footer

--- app/js/common.js
+++ app/js/common.js
@@ -3,3 +3,23 @@ $(function() {
 	// Custom JS
 
 });
+//=============================scroll on form=============================
+$(function() {
+
+    $(document).ready(function(){
+        $(".button-mobile").on("click","a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+
+            //забираем идентификатор бока с атрибута href
+            var id  = $(this).attr('href'),
+
+                //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;
+
+            //анимируем переход на расстояние - top за 1500 мс
+            $('body,html').animate({scrollTop: top}, 500);
+        });
+    });
+
+});
